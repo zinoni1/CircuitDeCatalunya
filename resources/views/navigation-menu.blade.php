7c843913452301ddae8c27679799149aab314cea
mohervictor <victorcrackball@gmail.com>
2024-05-17 17:29:19 +0200

    menuecho

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -11,12 +11,28 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+              <!-- Navigation Links -->
+              <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('tipo-averias.index') }}" :active="request()->routeIs('tipo-averias.index')">
+                        Ver Tipo de Averías
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('averias.index') }}" :active="request()->routeIs('averias.index')">
+                        Ver Incidencias
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('zonas.index')}}" :active="request()->routeIs('zonas.index')">
+                        Ver Zonas
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('cargos.index')}}" :active="request()->routeIs('cargos.index')">
+                        Ver Cargos
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('sectors.index') }}" :active="request()->routeIs('sectors.index')">
+                        Ver Sectores
+                    </x-nav-link>
                 </div>
-            </div>
+ 
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Teams Dropdown -->
