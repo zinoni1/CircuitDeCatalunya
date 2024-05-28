fea811e4c9c8b40b3062eb76f5d90de0133ec06a
mohervictor <victorcrackball@gmail.com>
2024-05-28 15:22:27 +0200

    rutas_arregladas

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\AveriasAnonimasController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('averiasAnonimas.index');
 });
 
 Route::middleware([
